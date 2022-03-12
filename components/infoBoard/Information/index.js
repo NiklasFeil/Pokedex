5aacba4257d2ff87d50570beaaf482fa224cb2c2
Niklas Feil <niklasfeil01@gmail.com>
2022-03-12 15:26:49 +0100

    AboutSection Component

--- components/infoBoard/Information/index.js
+++ components/infoBoard/Information/index.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { View, Text, ScrollView } from 'react-native'
+import AboutSection from './AboutSection';
 import styles from './styles';
 
 const Information = (props) => {
 
-    const entryTextContent = "CHARIZARD flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.";
-
     return(
         <View style={styles.container}>
             <ScrollView style={styles.textContainer}>
-                <Text style={styles.entryText}>
-                    {entryTextContent}
-                </Text>
+                <AboutSection />
             </ScrollView>
         </View>
     )
