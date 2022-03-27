4f548a37fe50896d223e763dc137545a478f7f56
Niklas Feil <niklasfeil01@gmail.com>
2022-03-27 22:43:23 +0200

    AboutSection continued

--- components/infoBoard/Information/AboutSection/index.js
+++ components/infoBoard/Information/AboutSection/index.js
@@ -4,7 +4,9 @@ import styles from "./styles"
 
 const AboutSection = (props) => {
 
-    const entryTextContent = "CHARIZARD flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.";
+    const entryTextContent = "CHARIZARD flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself    //Daten von Ditto
+    const pokemonHeight = 3;
+    const pokemonWeight = 40;
 
     return (
         <View style={styles.container}>
@@ -16,18 +18,18 @@ const AboutSection = (props) => {
             <View style={styles.physiqueContainer}>
                 <View style={styles.heightContainer}>
                     <Text style={styles.heightTitleText}>
-                        {/*In seperaten Container packen*/}
+                        Height
                     </Text>
                     <Text style={styles.heightText}>
-
+                        {pokemonHeight / 10} m
                     </Text>
                 </View>
                 <View style={styles.weightContainer}>
                     <Text style={styles.weightTitleText}>
-
+                        Weight
                     </Text>
                     <Text style={styles.weightText}>
-
+                        {pokemonWeight / 10} kg
                     </Text>
                 </View>
             </View>
