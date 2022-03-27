379450cadbfe37b1475738989ed4bb90d8417232
Niklas Feil <niklasfeil01@gmail.com>
2022-03-27 01:46:59 +0100

    AboutSection erweitert

--- components/infoBoard/Information/AboutSection/styles.js
+++ components/infoBoard/Information/AboutSection/styles.js
@@ -4,9 +4,33 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "red",
     },
+    entryTextContainer: {
+
+    }, 
     entryText: {
         fontSize: 12,
     },
+    physiqueContainer: {
+
+    },
+    heightContainer: {
+
+    },
+    heightTitleText: {
+
+    },
+    heightText: {
+
+    },
+    weightContainer: {
+
+    },
+    weightTitleText: {
+
+    },
+    weightText: {
+        
+    }
 });
 
 export default styles;
